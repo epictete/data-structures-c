2b824e479caed51a6c324602ca6c16a7f58295f4
John (jol) <jol@odoo.com>
2021-03-04 21:42:49 +0100

    BST Min & Max

---
+++ trees/bst-min-max.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}
+        Node;
+
+Node* GetNewNode(int data)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+
+    return newNode;
+}
+
+Node* Insert(Node* root, int data)
+{
+    if (root == NULL)
+        root =  GetNewNode(data);
+    else if (data <= root->data)
+        root->left = Insert(root->left, data);
+    else
+        root->right = Insert(root->right, data);
+    return root;
+}
+
+int Search(Node* root, int data)
+{
+    if (root == NULL) return 0;
+    else if (root->data == data) return 1;
+    else if (data <= root->data) return Search(root->left, data);
+    else return Search(root->right, data);
+}
+
+int FindMin(Node* root) // Iterative approach
+{
+    if (root == NULL)
+    {
+        printf("Error : tree is empty\n");
+        return -1;
+    }
+
+    while (root->left != NULL)
+        root = root->left;
+
+    return root->data;
+}
+
+int FindMax(Node* root) // Recursive approach
+{
+    if (root == NULL)
+    {
+        printf("Error : tree is empty\n");
+        return -1;
+    }
+
+    if (root->right == NULL)
+        return root->data;
+
+    return FindMax(root->right);
+}
+
+int main(void)
+{
+    Node* root = NULL;
+
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    root = Insert(root, 17);
+
+    printf("Minimum : %d\n", FindMin(root));
+    printf("Maximum : %d\n", FindMax(root));
+
+    return 0;
+}
